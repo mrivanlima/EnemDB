e7c03b46d9663610ca81bd580cf64621787ef9b3
TheKauanLima <thekauanlima@gmail.com>
2025-05-01 14:07:49 -0400

    test 5

--- app/Types/account_record.sql
+++ app/Types/account_record.sql
@@ -3,7 +3,7 @@ CREATE TYPE app.account_record AS
 (
     user_id int,
 	user_unique_id uuid,
-	username varchar(100),
+	username varchar(100), 
     email varchar(100),
 	password_hash varchar(100),
 	password_salt varchar(100),
