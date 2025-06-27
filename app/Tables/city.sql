145586a72fd7c52e0536b837cac162154d3e6585
Ivan Lima <mrivanlima@gmail.com>
2025-06-27 17:51:09 -0400

    City

--- app/Tables/city.sql
+++ app/Tables/city.sql
@@ -1,15 +1,16 @@
-CREATE TABLE IF NOT EXISTS app.city (
-    city_id               SERIAL,
-    state_id              INT NOT NULL,
-    city_name             TEXT NOT NULL,
-    city_name_friendly    TEXT NOT NULL,
-    created_by            TEXT NOT NULL,
-    created_on            TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    modified_by           TEXT,
-    modified_on           TIMESTAMPTZ,
+CREATE TABLE IF NOT EXISTS app.booklet_color (
+    booklet_color_id           SERIAL,
+    booklet_color_name         TEXT NOT NULL,
+    booklet_color_name_friendly TEXT NOT NULL,
+    is_accessible              BOOLEAN NOT NULL DEFAULT FALSE,
+    sort_order                 SMALLINT NOT NULL DEFAULT 0,
+    active                     BOOLEAN NOT NULL DEFAULT TRUE,
+    created_by                 TEXT NOT NULL,
+    created_on                 TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    modified_by                TEXT,
+    modified_on                TIMESTAMPTZ,
     -- Constraints
-    CONSTRAINT pk_city_id PRIMARY KEY (city_id),
-    CONSTRAINT fk_state_id FOREIGN KEY (state_id) REFERENCES app.state(state_id) ON DELETE CASCADE,
-    CONSTRAINT uq_city_state UNIQUE (state_id, city_name),
-    CONSTRAINT uq_city_state_friendly UNIQUE (state_id, city_name_friendly)
+    CONSTRAINT pk_booklet_color_id PRIMARY KEY (booklet_color_id),
+    CONSTRAINT uq_booklet_color_name UNIQUE (booklet_color_name),
+    CONSTRAINT uq_booklet_color_name_friendly UNIQUE (booklet_color_name_friendly)
 );
