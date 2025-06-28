284d020bc31e04d66a113272bcf6ca998211a731
Ivan Lima <mrivanlima@gmail.com>
2025-06-28 15:23:42 -0400

    updae master

--- DropTable.sql
+++ DropTable.sql
@@ -10,6 +10,86 @@
 -- drop table if exists imp.enem_cutoff_scores_2010_2018 cascade;
 -- drop table if exists imp.enem_cutoff_scores_2019_2024 cascade;
 
+drop table if exists app.academic_organization cascade;
+drop table if exists app.account cascade;
+drop table if exists app.address cascade;
+drop table if exists app.alternative cascade;
+drop table if exists app.answer_key cascade;
+drop table if exists app.answer_submission cascade;
+drop table if exists app.approved_student cascade;
+drop table if exists app.booklet_color cascade;
+drop table if exists app.booklet_mapping cascade;
+drop table if exists app.city cascade;
+drop table if exists app.degree_level cascade;
+drop table if exists app.degree cascade;
+drop table if exists app.error_log cascade;
+drop table if exists app.exam_attempt cascade;
+drop table if exists app.frequency cascade;
+drop table if exists app.item_statistics cascade;
+drop table if exists app.question cascade;
+drop table if exists app.quota_type cascade;
+drop table if exists app.region cascade;
+drop table if exists app.response cascade;
+drop table if exists app.seats cascade;
+drop table if exists app.shift cascade;
+drop table if exists app.special_quota cascade;
+drop table if exists app.state cascade;
+drop table if exists app.student_ability_estimation cascade;
+drop table if exists app.submission_deadline cascade;
+drop table if exists app.university_campus cascade;
+drop table if exists app.university_category cascade;
+drop table if exists app.university.sql cascade;
+drop table if exists app.year cascade;
+
+
+
+drop function if exists app.set_academic_organization_name_friendly() cascade;
+drop function if exists app.set_alternative_friendly_fields() cascade;
+drop function if exists app.set_booklet_color_name_friendly() cascade;
+drop function if exists app.set_city_name_friendly() cascade;
+drop function if exists app.set_degree_level_name_friendly() cascade;
+drop function if exists app.set_degree_name_friendly() cascade;
+drop function if exists app.set_exam_attempt_friendly_fields() cascade;
+drop function if exists app.set_frequency_name_friendly() cascade;
+drop function if exists app.set_question_friendly_fields() cascade;
+drop function if exists app.set_region_name_friendly() cascade;
+drop function if exists app.set_response_friendly_fields() cascade;
+drop function if exists app.set_shift_name_friendly() cascade;
+drop function if exists app.set_state_name_friendly() cascade;
+drop function if exists app.set_university_campus_name_friendly() cascade;
+drop function if exists app.set_university_category_name_friendly() cascade;
+drop function if exists app.set_university_name_friendly() cascade;
+drop function if exists app.usp_api_account_read_by_name() cascade;
+
+
+drop procedure if exists app.usp_api_degree_create() cascade;
+drop procedure if exists app.usp_api_academic_organization_create() cascade;
+drop procedure if exists app.usp_api_account_create() cascade;
+drop procedure if exists app.usp_api_address_create() cascade;
+drop procedure if exists app.usp_api_alternative_create() cascade;
+drop procedure if exists app.usp_api_answer_key_create() cascade;
+drop procedure if exists app.usp_api_answer_submission_create() cascade;
+drop procedure if exists app.usp_api_approved_student_create() cascade;
+drop procedure if exists app.usp_api_booklet_color_create() cascade;
+drop procedure if exists app.usp_api_booklet_mapping_create() cascade;
+drop procedure if exists app.usp_api_city_create() cascade;
+drop procedure if exists app.usp_api_degree_level_create() cascade;
+drop procedure if exists app.usp_api_exam_attempt_create() cascade;
+drop procedure if exists app.usp_api_frequency_create() cascade;
+drop procedure if exists app.usp_api_question_create() cascade;
+drop procedure if exists app.usp_api_quota_type_create() cascade;
+drop procedure if exists app.usp_api_region_create() cascade;
+drop procedure if exists app.usp_api_response_create() cascade;
+drop procedure if exists app.usp_api_shift_create() cascade;
+drop procedure if exists app.usp_api_special_quota_create() cascade;
+drop procedure if exists app.usp_api_state_create() cascade;
+drop procedure if exists app.usp_api_submission_deadline_create() cascade;
+drop procedure if exists app.usp_api_university_campus_create() cascade;
+drop procedure if exists app.usp_api_university_category_create() cascade;
+drop procedure if exists app.usp_api_university_create() cascade;
+drop procedure if exists app.usp_api_year_create() cascade;
+
+
 
 
 
--- master_script.sql
+++ master_script.sql
@@ -38,9 +38,41 @@
 
 --Add app tables
 
+\i './app/Tables/academic_organization.sql'
 \i './app/Tables/account.sql'
 \i './app/Tables/address.sql'
+\i './app/Tables/degree_level.sql'
+\i './app/Tables/degree.sql'
+\i './app/Tables/frequency.sql'
+\i './app/Tables/quota_type.sql'
+\i './app/Tables/region.sql'
+\i './app/Tables/state.sql'
+\i './app/Tables/city.sql'
+\i './app/Tables/university_category.sql'
+\i './app/Tables/university.sql'
+\i './app/Tables/university_campus.sql'
+\i './app/Tables/year.sql'
+\i './app/Tables/booklet_color.sql'
 \i './app/Tables/error_log.sql'
+\i './app/Tables/special_quota.sql'
+\i './app/Tables/item_statistics.sql'
+\i './app/Tables/student_ability_estimation.sql'
+\i './app/Tables/approved_student.sql'
+\i './app/Tables/submission_deadline.sql'
+\i './app/Tables/exam_attempt.sql'
+\i './app/Tables/question.sql'
+\i './app/Tables/alternative.sql'
+\i './app/Tables/booklet_mapping.sql'
+\i './app/Tables/answer_key.sql'
+\i './app/Tables/seats.sql'
+\i './app/Tables/answer_submission.sql'
+\i './app/Tables/response.sql'
+
+
+
+
+
+--Add stg Table
 \i './stg/Tables/campus.sql'
 \i './stg/Tables/cutoff_score.sql'
 \i './stg/Tables/organization.sql'
@@ -50,22 +82,89 @@
 \i './stg/Tables/shift.sql'
 \i './stg/Tables/course.sql'
 
+
 --Add app functions
+\i './app/Functions/set_academic_organization_name_friendly.sql'
+\i './app/Functions/set_alternative_friendly_fields.sql'
+\i './app/Functions/set_booklet_color_name_friendly.sql'
+\i './app/Functions/set_city_name_friendly.sql'
+\i './app/Functions/set_degree_level_name_friendly.sql'
+\i './app/Functions/set_degree_name_friendly.sql'
+\i './app/Functions/set_exam_attempt_friendly_fields.sql'
+\i './app/Functions/set_frequency_name_friendly.sql'
+\i './app/Functions/set_question_friendly_fields.sql'
+\i './app/Functions/set_region_name_friendly.sql'
+\i './app/Functions/set_response_friendly_fields.sql'
+\i './app/Functions/set_shift_name_friendly.sql'
+\i './app/Functions/set_state_name_friendly.sql'
+\i './app/Functions/set_university_campus_name_friendly.sql'
+\i './app/Functions/set_university_category_name_friendly.sql'
+\i './app/Functions/set_university_name_friendly.sql'
+\i './app/Functions/usp_api_account_read_by_name.sql'
+
 
 
 
 --Add Stored Procedures
 
 
+-- \i './app/StoredProcedures/usp_api_account_create.sql'
+-- \i './app/StoredProcedures/usp_api_address_create.sql'
+-- \i './stg/StoredProcedures/usp_transform_campus.sql'
+-- \i './stg/StoredProcedures/usp_transform_organization.sql'
+-- \i './stg/StoredProcedures/usp_transform_university.sql'
+-- \i './stg/StoredProcedures/usp_transform_category.sql'
+-- \i './stg/StoredProcedures/usp_transform_degree.sql'
+-- \i './stg/StoredProcedures/usp_transform_shift.sql'
+-- \i './stg/StoredProcedures/usp_transform_course.sql'
+
+\i './app/StoredProcedures/sp_api_degree_create.sql'
+\i './app/StoredProcedures/usp_api_academic_organization_create.sql'
 \i './app/StoredProcedures/usp_api_account_create.sql'
 \i './app/StoredProcedures/usp_api_address_create.sql'
-\i './stg/StoredProcedures/usp_transform_campus.sql'
-\i './stg/StoredProcedures/usp_transform_organization.sql'
-\i './stg/StoredProcedures/usp_transform_university.sql'
-\i './stg/StoredProcedures/usp_transform_category.sql'
-\i './stg/StoredProcedures/usp_transform_degree.sql'
-\i './stg/StoredProcedures/usp_transform_shift.sql'
-\i './stg/StoredProcedures/usp_transform_course.sql'
+\i './app/StoredProcedures/usp_api_alternative_create.sql'
+\i './app/StoredProcedures/usp_api_answer_key_create.sql'
+\i './app/StoredProcedures/usp_api_answer_submission_create.sql'
+\i './app/StoredProcedures/usp_api_approved_student_create.sql'
+\i './app/StoredProcedures/usp_api_booklet_color_create.sql'
+\i './app/StoredProcedures/usp_api_booklet_mapping_create.sql'
+\i './app/StoredProcedures/usp_api_city_create.sql'
+\i './app/StoredProcedures/usp_api_degree_level_create.sql'
+\i './app/StoredProcedures/usp_api_exam_attempt_create.sql'
+\i './app/StoredProcedures/usp_api_frequency_create.sql'
+\i './app/StoredProcedures/usp_api_question_create.sql'
+\i './app/StoredProcedures/usp_api_quota_type_create.sql'
+\i './app/StoredProcedures/usp_api_region_create.sql'
+\i './app/StoredProcedures/usp_api_response_create.sql'
+\i './app/StoredProcedures/usp_api_shift_create.sql'
+\i './app/StoredProcedures/usp_api_special_quota_create.sql'
+\i './app/StoredProcedures/usp_api_state_create.sql'
+\i './app/StoredProcedures/usp_api_submission_deadline_create.sql'
+\i './app/StoredProcedures/usp_api_university_campus_create.sql'
+\i './app/StoredProcedures/usp_api_university_category_create.sql'
+\i './app/StoredProcedures/usp_api_university_create.sql'
+\i './app/StoredProcedures/usp_api_year_create.sql'
+
+
+--Add app triggers
+\i './app/Triggers/trg_set_academic_organization_name_friendly.sql'
+\i './app/Triggers/trg_set_alternative_friendly_fields.sql'
+\i './app/Triggers/trg_set_booklet_color_name_friendly.sql'
+\i './app/Triggers/trg_set_city_name_friendly.sql'
+\i './app/Triggers/trg_set_degree_level_name_friendly.sql'
+\i './app/Triggers/trg_set_degree_name_friendly.sql'
+\i './app/Triggers/trg_set_exam_attempt_friendly_fields.sql'
+\i './app/Triggers/trg_set_frequency_name_friendly.sql'
+\i './app/Triggers/trg_set_question_friendly_fields.sql'
+\i './app/Triggers/trg_set_region_name_friendly.sql'
+\i './app/Triggers/trg_set_response_friendly_fields.sql'
+\i './app/Triggers/trg_set_shift_name_friendly.sql'
+\i './app/Triggers/trg_set_state_name_friendly.sql'
+\i './app/Triggers/trg_set_university_campus_name_friendly.sql'
+\i './app/Triggers/trg_set_university_category_name_friendly.sql'
+\i './app/Triggers/trg_set_university_name_friendly.sql'
+\i './app/Triggers/trg_set_year_name_friendly.sql'
+
 
 --Add Views
 
